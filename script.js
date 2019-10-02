99a4981c8ebe577d7d9c87b66e84fa2e55d180a3
Ajitesh13 <aji.yash13@gmail.com>
2019-10-02 12:27:13 -0400

    js added

---
+++ script.js
@@ -0,0 +1,8 @@
+var image = document.getElementById("image");
+image.addEventListener("mouseover", function(){
+    this.width = "400"
+});
+
+image.addEventListener("mouseout", function(){
+    this.width = "350"
+});
